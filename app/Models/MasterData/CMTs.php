dd308487b544446ed40b0295618bd1f88b93280d
timothhhyf <timf270502@gmail.com>
2025-10-29 00:20:23 +0700

    add models and adjustment migrations

---
+++ app/Models/MasterData/CMTs.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\MasterData;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CMTs extends Model
+{
+    /** @use HasFactory<\Database\Factories\MasterData\CMTsFactory> */
+    use HasFactory;
+}
