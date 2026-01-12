e8004e451b8d9ee0d389f80323c01158496f2d5a
William <william@is-gs.com>
2026-01-12 19:53:08 +0700

    restore lost request controller

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Transaction\RequestController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\LoginController;
