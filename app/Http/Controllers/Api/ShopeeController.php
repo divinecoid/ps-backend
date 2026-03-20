1f97fb90db96a5a4f81486de2709f30156afa34f
William <william@is-gs.com>
2026-03-21 04:42:07 +0700

    expose file name

--- app/Http/Controllers/Api/ShopeeController.php
+++ app/Http/Controllers/Api/ShopeeController.php
@@ -363,6 +363,7 @@ class ShopeeController extends Controller
 
             // Return as downloadable PDF
             return response($fileContent)
+                ->header('Access-Control-Expose-Headers', 'Content-Disposition')
                 ->header('Content-Type', 'application/pdf')
                 ->header('Content-Disposition', 'attachment; filename="shipping_document_' . $request->order_sn . '.pdf"');
 
