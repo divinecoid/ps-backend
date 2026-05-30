9b2a30f4916dc5258f993224e957ffb7da1e8498
William <william@is-gs.com>
2026-05-30 07:58:08 +0700

    composite key fix for product model color sizes

--- app/Http/Controllers/MasterData/ProductModelController.php
+++ app/Http/Controllers/MasterData/ProductModelController.php
@@ -112,8 +112,8 @@ class ProductModelController extends Controller
                 ]
             ],
             function (ProductModel $model, Request $req) {
-                $model->sizes()->attach($req->size_id);
-                $model->colors()->attach($req->color_id);
+                $model->sizes()->syncWithoutDetaching([$req->size_id]);
+                $model->colors()->syncWithoutDetaching([$req->color_id]);
             }
         );
     }
