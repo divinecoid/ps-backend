c1424ec885a19c6bcb86caf2d791ef569abc1729
William <william@is-gs.com>
2026-05-31 00:12:56 +0700

    fix: product model controller

--- app/Http/Controllers/MasterData/ProductModelController.php
+++ app/Http/Controllers/MasterData/ProductModelController.php
@@ -112,8 +112,8 @@ class ProductModelController extends Controller
                 ]
             ],
             function (ProductModel $model, Request $req) {
-                $model->sizes()->syncWithoutDetaching($req->size_id);
-                $model->colors()->syncWithoutDetaching($req->color_id);
+                $model->sizes()->sync($req->size_id);
+                $model->colors()->sync($req->color_id);
             }
         );
     }
