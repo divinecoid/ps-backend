a6b939e04825b0466b3f976da332f117ff812a87
William <william@is-gs.com>
2026-07-03 19:17:06 +0700

    database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -38,7 +38,6 @@ class DatabaseSeeder extends Seeder
             ProductModelSeeder::class,
             WarehouseRackSeeder::class,
             ShopeeSeeder::class,
-            WarehouseRackSeeder::class,
             ConfigurationSeeder::class,
             ShippingLogisticSeeder::class,
             FactorySeeder::class,
@@ -60,7 +59,6 @@ class DatabaseSeeder extends Seeder
                 'factory_id' => $factory->id,
                 'gram' => '200',
                 'roll_size_id' => $rollSize->id,
-                'color_id' => $color->id,
                 'quantity' => 100,
                 'sequence' => 'K1'
             ]);
