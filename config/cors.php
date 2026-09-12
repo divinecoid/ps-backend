1c3bef1d79bf5ae93ef5abfc22dc5b6cb07c9fb9
divinecoid <admin@divine.co.id>
2026-09-12 13:03:49 +0700

    cors

--- config/cors.php
+++ config/cors.php
@@ -15,6 +15,8 @@ return [
         'http://127.0.0.1:8000',
         'http://127.0.0.1:3000',
         'http://127.0.0.1:5173',
+        'http://156.67.219.209:8091',
+        'https://ps.divineproject.my.id'
     ],
 
     'allowed_origins_patterns' => [],
